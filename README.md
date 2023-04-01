d7c4f32f8a888148115fcf8bc02e64cac59f2e00
wentao709 <wentaozhouuofa@gmail.com>
2023-04-01 10:28:05 -0700

    blcok explorer

---
+++ README.md
@@ -0,0 +1,7 @@
+# Ethereum Block Explorer
+
+Start the webserver
+
+`npm start`
+
+Running the command above will run the app in the development mode.
---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { Alchemy, Network } from 'alchemy-sdk';
+import { useEffect, useState } from 'react';
+
+import './App.css';
+
+const settings = {
+  apiKey: process.env.REACT_APP_ALCHEMY_API_KEY,
+  network: Network.ETH_MAINNET,
+};
+
+const alchemy = new Alchemy(settings);
+
+function App() {
+  const [blockNumber, setBlockNumber] = useState();
+  const [block, setBlock] = useState();
+
+  useEffect(() => {
+    async function getBlockNumber() {
+
+      setBlockNumber(await alchemy.core.getBlockNumber());
+      setBlock(await alchemy.core.getBlock());
+    }
+    console.log(block);
+    getBlockNumber();
+  });
+
+  return <div className="App">Block Number: {blockNumber}</div>;
+}
+
+export default App;
